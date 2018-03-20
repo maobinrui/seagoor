93b1fb5ed8388ce38151e768da6eed4c683d26c8
smalllv <2325611151@qq.com>
2018-03-20 09:37:33 +0800

    test

---
+++ seagoor/src/router/index.js
@@ -0,0 +1,55 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+
+import Home from "../components/home";//home 页
+import Range from "../components/range";// 分类页
+import Detail from "../components/detail";//注册
+import Shoppingcar from "../components/shoppingcar";//购物车
+import Personal from "../components/personal";//个人中心
+import Login from "../components/login";//登录
+import Register from "../components/register";//注册
+
+
+Vue.use(Router)
+
+const router =  new Router({
+  mode:"hash",
+
+  routes: [
+    {
+        path:"/home",
+
+        component:Home
+    },
+    {
+        path:"/range",
+        component:Range,
+    },
+    {
+        path:"/shoppingcar",
+        component:Shoppingcar,
+    },
+    {
+        path:"/personal",
+        component:Personal
+    },
+    {
+        path:"/login",
+        component:Login
+    },
+    {
+        path:"/register",
+        component:Register
+    },
+    {
+        name:"detail",
+        path:"/detail/:id",
+        component:Detail
+    },
+    {
+        path:"*", //通配符
+        redirect:"/home"
+    }
+  ]
+})
